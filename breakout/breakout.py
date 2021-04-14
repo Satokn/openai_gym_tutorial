cf725b50274a1351b0ea47fabbc72a8c1ffb1b00
Kento Sato <satokn.exe@gmail.com>
2021-04-14 14:03:09 +0900

    fix: test model

--- breakout/breakout.py
+++ breakout/breakout.py
@@ -4,7 +4,8 @@ from stable_baselines import PPO2
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.common import set_global_seeds
 from stable_baselines.bench import Monitor
-from utils_for_upd import SaveOnBestTrainingRewardCallback, log_dir
+from baselines.common.atari_wrappers import *
+from util import SaveOnBestTrainingRewardCallback, log_dir
 
 ENV_ID = "BreakoutNoFrameskip-v0"
 NUM_ENV = 8
@@ -12,6 +13,13 @@ NUM_ENV = 8
 def make_env(env_id, rank, seed=0):
     def _init():
         env = gym.make(env_id)
+        env = NoopResetEnv(env, noop_max=30)
+        env = MaxAndSkipEnv(env, skip=4)
+        env = FireResetEnv(env)
+        env = WarpFrame(env)
+        env = ScaledFloatFrame(env)
+        env = ClipRewardEnv(env)
+        env = EpisodicLifeEnv(env)
         if rank == 0:
             env = Monitor(env, log_dir, allow_early_resets=True)
         env.seed(seed + rank)
@@ -23,9 +31,9 @@ def make_env(env_id, rank, seed=0):
 def main():
     train_env = DummyVecEnv([make_env(ENV_ID, i) for i in range(NUM_ENV)])
     model = PPO2("CnnPolicy", train_env, verbose=0)
-    # model = PPO2.load("breakout_model", env=train_env, verbose=0)
+    model = PPO2.load("./logs/best_model.zip", env=train_env, verbose=0)
     callback = SaveOnBestTrainingRewardCallback(check_freq=10, log_dir=log_dir)
-    model.learn(total_timesteps=1280000, callback=callback)
+    #model.learn(total_timesteps=1280000, callback=callback)
 
     test_env = DummyVecEnv([make_env(ENV_ID, 9)])
 
--- breakout/util.py
+++ breakout/util.py
@@ -1,30 +1,52 @@
-import gym
 import os
+
+import gym
 import numpy as np
-import datetime
-import pytz
+import matplotlib.pyplot as plt
+
+from stable_baselines import DDPG
+from stable_baselines.ddpg.policies import LnMlpPolicy
+from stable_baselines import results_plotter
+from stable_baselines.bench import Monitor
 from stable_baselines.results_plotter import load_results, ts2xy
+from stable_baselines.common.noise import AdaptiveParamNoiseSpec
+from stable_baselines.common.callbacks import BaseCallback
+
 
 log_dir = "./logs/"
 os.makedirs(log_dir, exist_ok=True)
 
-num_update = 0
-best_mean_reward = -np.inf
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    def __init__(self, check_freq: int, log_dir: str, verbose=1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model')
+        self.best_mean_reward = -np.inf
 
-def callback(_locals, _globals):
-    global num_update
-    global best_mean_reward
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
 
-    if (num_update + 1) % 10 == 0:
-        _, y = ts2xy(load_results(log_dir), "timesteps")
-        if len(y) > 0:
-            mean_reward = np.mean(y[-10:])
-            update_model = mean_reward > best_mean_reward
-            if update_model:
-                best_mean_reward = mean_reward
-                model.save('breakout_model')
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
 
-            print(f"time: {datetime.datetime.now()}, num_update: {num_update}, mean: {mean_reward}, best_mean: {best_mean_reward}, model_update: {update_model}")
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose > 0:
+                print("Num timesteps: {}".format(self.num_timesteps))
+                print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(self.best_mean_reward, mean_reward))
 
-    num_update += 1
-    return True
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose > 0:
+                    print("Saving new best model to {}".format(self.save_path))
+                  self.model.save(self.save_path)
+
+        return True
